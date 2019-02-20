43106068354ca61db908d8110ba9bdb82124b383
Mzoir_zakariya <47563024+mzoir@users.noreply.github.com>
2019-02-21 00:56:46 +0100

    tools-termux.py

---
+++ tools-termux.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+from time import *
+import mechanize
+
+purpleClaro = "\033[1;35m"
+cyanClaro = "\033[1;36m"
+vermelho = "\033[31;1m"
+amarelo = "\033[1;33m"
+magenta = "\033[45m"
+normal = "\033[0;0m"
+verde = "\033[32;1m"
+azul = "\033[34;1m"
+branco = "\033[0m"
+ciano = "\033[46m"
+
+os.system("figlet MZOIR")
+os.system("figlet ZAKARIYA")
+                                
+print vermelho+"""
+######################MZOIR ZAKARIYA ###############
+OUR CHANNELS IN YOUTUBE = 
+
+############ WRITTEN AND FOUNDED ###################
+
+########### ALL BEST TERMUX TOOLS##################
+
+#######################MZOIR ZAKARIYA##############
+"""
+
+os.system("figlet TERMUX")
+os.system("figlet TOOLS:")
+
+print verde+"""HELLO ALL FRIEND MY TOOLS  FOUNDED  
+FOR  KNOWING THE BEST TOOLS IMPORTANT FOR THE BIGGENERS 
+HACKERS IT'S EASY TO USING;CHOICE YOUR TOOL NOW
+"""
+
+
+if sys.version_info [0] !=2:
+  print" WE ARE SORRY THIS  TOOL BASED ON PYTHON2 TRY TO USE IT PLEASE "
+  exit()                                          
+os.system("figlet MAIN");print("founded by mzoir")
+main ="""print {1} nano 
+print {2} wget 
+print {3} curl 
+print {4} mtools"""
+
+print(main)
+
+r = raw_input(" '''>>> ")
+            
+if    r == "1" :
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      print("starting installing .....") 
+      os.system("pkg install nano ")
+        
+elif  r == "2" :
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      print("starting installing .....")
+      os.system("pkg install wget")
+
+elif r == "3":
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      print("starting installing .....")
+      os.system("pkg install curl ")
+elif r == "4":
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      os.system("git clone  https://github.com/mzoir/mind_game")
+else :
+  print("goodbye ")
+  exit()
+r  = raw_input(" '''>>> ")
+    
+
+if    r == "2" :
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      print("starting installing .....")
+      os.system("pkg install wget")      
+
+elif r == "3":
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      print("starting installing .....")
+      os.system("pkg install curl ")
+elif r == "4":
+      print("starting installing.")
+      sleep = 0.2
+      print("starting installing ...")
+      sleep = 0.2
+      os.system("git clone  https://github.com/mzoir/mind_game")
+else:
+  print("goodbye ")
+  exit()
+
+
+
